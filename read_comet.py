98cc01fd10c8af6f71706f41ed45ca544d7dcd3b
Victor Lin <bornstub@gmail.com>
2010-10-27 01:26:07 +0800

    Add two simple tool for testing comet server

---
+++ read_comet.py
@@ -0,0 +1,18 @@
+# A simple comet client for debugging
+import sys
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send('GET /read HTTP/1.1\r\n\r\n')
+
+while True:
+    data = s.recv(1024)
+    if not data:
+        break
+    print repr(data)
+s.close()
+
+print 'Closed by peer'
---
+++ write_comet.py
@@ -0,0 +1,22 @@
+# A simple comet client for debugging
+import sys
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+content = 'Hello baby!'
+s.send('GET /write HTTP/1.1\r\nContent-Length: %d\r\n\r\n%s' % (
+    len(content), content
+))
+
+while True:
+    data = s.recv(1024)
+    if not data:
+        break
+    print repr(data)
+s.close()
+
+print 'Closed by peer'
