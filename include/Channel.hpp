e551a685688649645faa2239524817a7b5ab411d
Victor Lin <bornstub@gmail.com>
2010-10-30 16:48:08 +0800

    Implements some details of channel

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -1,12 +1,60 @@
 #ifndef __LITE_COMET_CHANNEL__HEADER__
 #define __LITE_COMET_CHANNEL__HEADER__
 
-#include <set>
+#include <list>
 
-#include <pion/net/WebService.hpp>
-#include <pion/net/HTTPResponseWriter.hpp>
+#include <boost/signals2.hpp>
+#include <boost/function.hpp>
 
 namespace lite_comet {
+
+class Message {
+private:
+    long m_offset;
+    std::string m_data;
+public:
+    Message(long offset, const std::string &data) 
+        : m_offset(offset), m_data(data)
+    {}
+
+    Message(const Message& other) {
+        setOffset(other.getOffset());
+        setData(other.getData());
+    }
+
+    /**
+        @brief get offset of this message
+        @return offset
+    **/
+    inline long getOffset() const {
+        return m_offset;
+    }
+
+    /**
+        @brief set offset of this message
+        @param offset: offset to set
+    **/
+    inline void setOffset(long offset) {
+        m_offset = offset;
+    }
+
+    /**
+        @brief get data of this message
+        @return data
+    **/
+    inline const std::string& getData() const {
+        return m_data;
+    }
+
+    /**
+        @brief set data of this message
+        @param data: data to set
+    **/
+    inline void setData(const std::string& data) {
+        m_data = data;
+    }
+  
+};
 
 /**
     Channel for registering event data
@@ -21,37 +69,61 @@ public:
     static const long WAITING_DATA = -1;
     ///
     static const long NEEDS_RESYNC = -3;
+
+    /// Container of messages
+    typedef std::list<Message> MessageList;
+    /// Signal for notifying listeners
+    typedef boost::signals2::signal<void ()> Signal;
+    /// Listener function type
+    typedef boost::function<void ()> Listener;
+private:
+    /// Messages of this channel
+    MessageList m_messages;
+    /// Event for data added
+    Signal m_data_added;
 public:
     Channel() {}
-    virtual ~Channel() {}
+    ~Channel() {}
 
     /**
         @brief Add data to channel
         @param data to add
     **/
-    virtual void addData(const std::string&) ;
+    void addData(const std::string&) ;
 
     /**
         @brief Add data to channel
         @param data to add
     **/
-    virtual const std::string getData() const ;
+    const std::string getData(long offset) ;
 
     /**
         @brief Add data to channel
         @param data to add
     **/
-    virtual long getCurrentOffset() const ;
+    long getCurrentOffset() const ;
 
     /**
         @brief Determine is this channel active
     **/
-    virtual bool isActive(/*current date*/) const ;
+    bool isActive(/*current date*/) const ;
 
     /**
         @brief Mark this channel as active
     **/
-    virtual void markActive() ;
+    void markActive() ;
+
+    /**
+        @brief Add listener to data added event
+    **/
+    void addListener(Listener) ;
+
+private:
+    /**
+        @brief Notify all listeners attached to this channel
+    **/
+    void notify() ;
+
 };
 
 }   // end namespace lite_comet
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include <boost/bind.hpp>
 #include <pion/net/HTTPResponseWriter.hpp>
 #include <pion/net/PionUser.hpp>
@@ -8,16 +10,87 @@ using namespace std;
 
 namespace lite_comet {
 
-void Channel::addData(const std::string&) {
-
+static string getResponse(long new_offset, const string& data = string("[]")) {
+    stringstream result;
+    result << "{" 
+        << "new_offset:" << new_offset<< ","
+        << "data:" << data
+    << "}";
+    return result.str();
 }
 
-const std::string Channel::getData() const {
-    return "";
+void Channel::addData(const std::string& data) {
+    long current_offset = getCurrentOffset();
+    // Reset the current offset if it is too big
+    if(current_offset > 65536) {
+        current_offset = 0;
+        m_messages.clear();
+    } else if (current_offset == WAITING_DATA) {
+        current_offset = 0;
+    }
+    ++current_offset;
+    m_messages.push_back(Message(current_offset, data));
+    if(m_messages.size() > 100) {
+        m_messages.pop_front();
+    }
+    notify();
+    markActive();
+}
+
+const std::string Channel::getData(long offset) {
+    markActive();
+
+    // We don't have data
+    if(m_messages.empty()) {
+        return getResponse(WAITING_DATA);
+    }
+
+    bool include_preceding = false;
+    // If we are waiting for data we return all data
+    if(offset == WAITING_DATA) {
+        include_preceding = true;
+    }
+    // Try to find the current offset
+    // if it's not found the data needs to be resynced
+    if(offset > 0) {
+        bool current_offset_found = false;
+        MessageList::const_iterator i = m_messages.begin();
+        for(; i != m_messages.end(); ++i) {
+            const Message& msg = *i;
+            if(msg.getOffset() == offset) {
+                current_offset_found = true;
+            }
+        }
+
+        if(!current_offset_found) {
+            return getResponse(NEEDS_RESYNC);
+        }
+    }
+
+    long new_offset = WAITING_DATA;
+    stringstream data;
+    data << "[";
+    MessageList::const_iterator i = m_messages.begin();
+    for(; i != m_messages.end(); ++i) {
+        const Message& msg = *i;
+        new_offset = msg.getOffset();
+        if(include_preceding) {
+            data << msg.getData();
+            MessageList::const_iterator j = ++i;
+            if(j != m_messages.end()) {
+                data << ",";
+            }
+        }
+    }
+    data << "]";
+    return getResponse(new_offset, data.str());
 }
 
 long Channel::getCurrentOffset() const {
-    return 0;
+    if(m_messages.empty()) {
+        return WAITING_DATA;
+    }
+    return m_messages.back().getOffset();
 }
 
 bool Channel::isActive(/*current date*/) const {
@@ -28,6 +101,14 @@ void Channel::markActive() {
 
 }
 
+void Channel::addListener(Listener func) {
+    m_data_added.connect(func);
+}
+
+
+void Channel::notify() {
+    m_data_added();
+}
 
 }   // end namespace lite_comet 
 
