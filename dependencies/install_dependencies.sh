d80f2d97491c6bd467c735918c02cb556765572f
Victor Lin <bornstub@gmail.com>
2010-10-31 03:01:27 +0800

    Remove log4cplus-1.0.4

--- dependencies/install_dependencies.sh
+++ dependencies/install_dependencies.sh
@@ -5,12 +5,7 @@ sudo apt-get install -y libboost-dev libboost-date-time-dev libboost-thread-dev 
 sudo apt-get install -y libbz2-dev libzip-dev openssl libxml2-dev
 
 # Install Log4CPlus
-wget http://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/1.0.4/log4cplus-1.0.4-rc10.tar.bz2
-
-tar xvf log4cplus-1.0.4-rc10.tar.bz2
-
 cd log4cplus-1.0.4-rc10
-
-./configure
-
+cmake -G "Unix Makefiles"
+make 
 sudo make install
